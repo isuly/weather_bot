f5cbb303e431538104c0dd99e1af96602aa87571
isuly <isulyshka@mail.ru>
2021-03-04 18:34:33 +0300

    add current weather request

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,11 +1,19 @@
 import telebot
 
+from src.weather import WeatherForecast
+
 bot = telebot.TeleBot('1689836173:AAEHmhMvTbRLslLvXVYh0WRJZD57q4L_2RE')
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+
+@bot.message_handler(commands=['weather'])
+def get_current_weather(message):
+    current_weather = WeatherForecast(city="Kazan").get_current_weather()
+    bot.send_message(message.chat.id, current_weather)
 
 
 bot.polling()
---
+++ src/weather.py
@@ -0,0 +1,10 @@
+import requests
+
+
+class WeatherForecast:
+
+    def __init__(self, city="Kazan"):
+        self.city = city
+
+    def get_current_weather(self):
+        pass
