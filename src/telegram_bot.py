3e3c7ffaebede049d7871e9aa496122d8b7438d0
isuly <isulyshka@mail.ru>
2021-03-05 17:01:16 +0300

    update bot messages

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -8,13 +8,16 @@ bot = telebot.TeleBot(config['bot_id'])
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Hi! Im weather bot! \nSend me "weather" to get current weather!')
 
 
-@bot.message_handler(commands=['weather'])
+@bot.message_handler(content_types=['text'])
 def get_current_weather(message):
-    current_weather = WeatherForecast(city="Kazan").get_current_weather()
-    bot.send_message(message.chat.id, current_weather)
+    if message.text.lower() == 'weather':
+        bot.send_message(message.chat.id, 'Send me your city')
+    else:
+        current_weather = WeatherForecast(city=message.text).get_current_weather()
+        bot.send_message(message.chat.id, current_weather)
 
 
 bot.polling()
