bd65240b6d5cce6fc6ca13dec1663f82df1a6239
isuly <isulyshka@mail.ru>
2021-03-05 10:58:48 +0300

    add working current weather request

--- src/weather.py
+++ src/weather.py
@@ -7,4 +7,7 @@ class WeatherForecast:
         self.city = city
 
     def get_current_weather(self):
-        pass
+        response = requests.get("http://api.openweathermap.org//data/2.5/weather",
+                                params={'q': self.city, 'APPID': '144a633e68a82d00dbecdd5e53d01104'})
+        if response.status_code == 200:
+            return f"Current temperature is {int(response.json()['main']['temp'] - 273)}°C"
