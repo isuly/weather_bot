5e4571f03d563825d843a459f30b96e52ad65baa
isuly <isulyshka@mail.ru>
2021-04-05 16:41:41 +0300

    add /current_weather command

--- src/app/telegram_bot.py
+++ src/app/telegram_bot.py
@@ -7,33 +7,35 @@ from app.weather import WeatherForecast
 bot = telebot.TeleBot(BOT_ID)
 
 
-# ToDo: обновить ридми
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Hi! Im weather bot! \nSend me "weather" to get current weather!')
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['current_weather'])
 def get_current_weather(message):
     user = get_user(message.chat.username)
-    if message.text.lower() == 'weather':
-        if user:
-            current_weather = WeatherForecast(city=user.city).get_current_weather()
-            if not current_weather:
-                bot.send_message(message.chat.id, 'Send me your city')
-            bot.send_message(message.chat.id, current_weather)
-        else:
-            new_user = create_user(message.chat.username, message.chat.name)
-            if new_user:
-                bot.send_message(message.chat.id, 'Send me your city')
-    else:
-        if user:
-            update_city(user, message.text)
-        else:
-            user = create_user(message.chat.username, message.chat.name, message.text)
+    if user:
         current_weather = WeatherForecast(city=user.city).get_current_weather()
         if not current_weather:
             bot.send_message(message.chat.id, 'Send me your city')
+        bot.send_message(message.chat.id, current_weather)
+    else:
+        new_user = create_user(message.chat.username, message.chat.name)
+        if new_user:
+            bot.send_message(message.chat.id, 'Send me your city')
+
+
+@bot.message_handler(content_types=['text'])
+def get_current_weather(message):
+    user = get_user(message.chat.username)
+    if user:
+        update_city(user, message.text)
+    else:
+        user = create_user(message.chat.username, message.chat.name, message.text)
+    current_weather = WeatherForecast(city=user.city).get_current_weather()
+    if not current_weather:
+        bot.send_message(message.chat.id, 'Send me your city')
 
 
 bot.polling()
